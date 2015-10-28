88fdddc71ab22d72716cb025991a525b26edce99
Cem Celebi <cem.celebi.paul@gmail.com>
2015-10-28 14:21:19 +0200

    client ile

--- odev04/message_client.py
+++ odev04/message_client.py
@@ -1,10 +1,26 @@
+__author__ = 'cemcelebi'
 import socket               # Import socket module
-
+import threading
+import time
 s = socket.socket()         # Create a socket object
-host = socket.gethostname() # Get local machine name
-print("host:",host)
-port = 5432                # Reserve a port for your service.
+class fromServer(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+    def run(self):
+        while True:
+            data=s.recv(1024)
+            if(data!= ""):
+                print("bosluk")
+            else:
+                print("server'in mesaji:"+data)
+                               
+    
 
+host = socket.gethostname() # Get local machine name
+port = 5432                # Reserve a port for your service.
 s.connect((host, port))
-print s.recv(1024)
-s.close   
+print("Client servisi..")
+#print s.recv(1024)
+threadLO=fromServer()
+threadLO.start()
+s.close()
