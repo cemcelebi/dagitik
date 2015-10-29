1ae48fb459428b17d5c1cb3de5275ba8007fdb1d
cemcelebi <cem.celebi.paul@gmail.com>
2015-10-29 16:12:30 +0200

    Update message_client.py

--- odev04/message_client.py
+++ odev04/message_client.py
@@ -2,25 +2,26 @@ __author__ = 'cemcelebi'
 import socket               # Import socket module
 import threading
 import time
-s = socket.socket()         # Create a socket object
+global sock
+sock = socket.socket()         # Create a socket object
 class fromServer(threading.Thread):
     def __init__(self):
         threading.Thread.__init__(self)
     def run(self):
+        global sock
         while True:
-            data=s.recv(1024)
+            data=sock.
             if(data!= ""):
                 print("bosluk")
             else:
-                print("server'in mesaji:"+data)
-                               
-    
+                print ("Server diyor ki \""+data+"\"")
 
 host = socket.gethostname() # Get local machine name
 port = 5432                # Reserve a port for your service.
-s.connect((host, port))
+sock.connect((host, port))
 print("Client servisi..")
 #print s.recv(1024)
 threadLO=fromServer()
 threadLO.start()
-s.close()
+sock.send("exiting client")
+sock.close()
