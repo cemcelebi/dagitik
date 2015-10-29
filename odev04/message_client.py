eb4b3a1b08fb226542d9d11d19114399d0a73492
cemcelebi <cem.celebi.paul@gmail.com>
2015-10-29 19:36:45 +0200

    Update message_client.py

--- odev04/message_client.py
+++ odev04/message_client.py
@@ -2,19 +2,19 @@ __author__ = 'cemcelebi'
 import socket               # Import socket module
 import threading
 import time
-global sock
+
 sock = socket.socket()         # Create a socket object
 class fromServer(threading.Thread):
     def __init__(self):
         threading.Thread.__init__(self)
     def run(self):
-        global sock
+
         while True:
-            data=sock.
+            data=sock.recv(1024)
             if(data!= ""):
                 print("bosluk")
             else:
-                print ("Server diyor ki \""+data+"\"")
+                print ("Server says: \""+data+"\"")
 
 host = socket.gethostname() # Get local machine name
 port = 5432                # Reserve a port for your service.
@@ -22,6 +22,9 @@ sock.connect((host, port))
 print("Client servisi..")
 #print s.recv(1024)
 threadLO=fromServer()
+#bu satiri oguz eroglu'ndan aldim. cok garip bir sekilde,
+#bu satir olmadan "bad file descriptor" hatasi aliyorum.
+threadLO.setDaemon(True)
 threadLO.start()
 sock.send("exiting client")
 sock.close()
