0fd557603ec48ea49346b1676ee70a263c21093c
cemcelebi <cem.celebi.paul@gmail.com>
2015-10-29 16:12:45 +0200

    Update message_server.py

--- odev04/message_server.py
+++ odev04/message_server.py
@@ -2,7 +2,10 @@ __author__ = 'cemcelebi'
 import socket               # Import socket module
 import threading
 import random
+import time
 threadCounter=0
+clients=[]
+
 class myThread (threading.Thread):
     def __init__(self, threadCounter, clientSocket, clientAddr):
         threading.Thread.__init__(self)
@@ -11,10 +14,20 @@ class myThread (threading.Thread):
         self.clientAddr = clientAddr
     def run(self):
         print "Starting Thread-" + str(self.threadCounter)
-        rand=random.randrange(5)
-        while rand>=0:
-            c.send("it's mon26oct2015")
-            rand=rand-1
+        #c.send("Peki from:" + str(self.clientAddr))
+        bool=False
+        while bool!=True:
+            data = c.recv(1024)
+            if data=="exiting client":
+                bool=True#cikis_yap
+                print("client cikmak istedi, kendi bilir..")
+            else:
+                c.send("Peki from:" + str(self.clientAddr))
+                #c.recv(1024)
+                for key in threads:
+                    #if threads==:
+                    c.send("Merhaba, su an saat "+time.strftime("%H:%M:%S"))
+                time.sleep(random.randrange(2,3))
         print "Ending Thread-" + str(self.threadCounter)
 
 
@@ -23,7 +36,9 @@ s = socket.socket()         # Create a socket object
 host = socket.gethostname() # Get local machine name
 port = 5432               # Reserve a port for your service.
 s.bind((host, port))
-threads=[]
+threads={} #dict of threads, T1:Actif;T2:Non-Actif;T3:Actif;T4:Actif vs..
+#activeThreads=[] #list of active threads
+
 print(host)
 # Bind to the port
 print("basladi")
@@ -33,52 +48,13 @@ while True:
     c, addr=s.accept()
     threadLO=myThread(threadCounter,c,addr)
     threadLO.start()
-   # c, addr = s.accept()     # Establish connection with client.
-    threads.append(threadLO)
+    #threads.append(threadLO)
     threading._sleep(0.00001)
-    print 'Got connection from', threadLO.clientAddr
-
-
+    print ('Got connection from', threadLO.clientAddr)
     c.send('Thank you for connecting')
+    threads[threadCounter]=threadLO
+    threads[c]="Actif"
     threadCounter=threadCounter+1
-
-threadLO.close()                # Close the connection
-__author__ = 'cemcelebi'
-import socket               # Import socket module
-import threading
-threadCounter=0
-class myThread (threading.Thread):
-    def __init__(self, threadCounter, clientSocket, clientAddr):
-        threading.Thread.__init__(self)
-        self.threadCounter = threadCounter
-        self.clientSocket = clientSocket
-        self.clientAddr = clientAddr
-    def run(self):
-        print "Starting Thread-" + str(self.threadCounter)
-
-        print "Ending Thread-" + str(self.threadCounter)
-
-
-
-s = socket.socket()         # Create a socket object
-host = socket.gethostname() # Get local machine name
-port = 5432               # Reserve a port for your service.
-s.bind((host, port))
-threads=[]
-print(host)
-# Bind to the port
-print("basladi")
-#s.listen(5)                 # Now wait for client connection.
-while True:
-    s.listen(5)
-    c, addr=s.accept()
-    threadLO=myThread(threadCounter,c,addr)
-    threadLO.start()
-   # c, addr = s.accept()     # Establish connection with client.
-    threads.append(threadLO)
-    threading._sleep(0.00001)
-    print 'Got connection from', threadLO.clientAddr
-    c.send('Thank you for connecting')
-    threadCounter=threadCounter+1
+    print("arr:",threads)
 
 threadLO.close()                # Close the connection
